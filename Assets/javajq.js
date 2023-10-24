7c171ec88e6175c925dd17e58e4f848265345191
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-10-24 17:43:36 -0500

    Yeah I'm Finnaly Donegit add -A!

--- Assets/javajq.js
+++ Assets/javajq.js
@@ -14,10 +14,18 @@ var winspe = 0;
 var place = '';
 var redo = $(`#redo`)
 var i=0;
-// var btn = $(`.btn`)
+var lat;
+var lon;
+var future = '';
+var futurecard = $('#futurecard')
 
 //Starting Sequence
-searchBTN.on('click',funky1)
+searchBTN.on('click',funky1);
+
+function conversion(ko) {
+    var fo = ((ko - 273.15) * (9 / 5) + 32).toFixed(2);
+    return fo;
+  }
 
 function funky1(){
 city = $(`#input`).val()
@@ -34,42 +42,57 @@ sunshine = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ap
         place = data.name;
         descrip = data.weather[0].description;
         ic = data.weather[0].icon;
-        tp = data.main.temp;
-        maxtemp = data.main.temp_max;
-        mintemp = data.main.temp_min;
+        icImg = `http://openweathermap.org/img/w/`+ic+`.png`;
+        tp = conversion(data.main.temp);
+        maxtemp = conversion(data.main.temp_max);
+        mintemp = conversion(data.main.temp_min);
         hum = data.main.humidity;
         winspe = data.wind.speed;
+        lat = data.coord.lat.toFixed(2);
+        lon = data.coord.lon.toFixed(2);
 
         infoCard.append(
-            `<div class="row align-items-start">
-                <div class="col">
-                    <h2>${place}</h2>
-                    <h2>${ic}</h2>
-                </div>
-            </div>
-            <div class="row align-items-start">
-                <div class="col">
-                    <h2>${descrip}</h2>
-                </div>
-            </div>
-            <div class="row align-items-start">
-                <div class="col">
-                    <h2>Temp: ${tp}</h2>
-                    <h2>MaxTemp: ${maxtemp}</h2>
-                    <h2>MinTemp: ${mintemp}</h2>
-                    <h2>Humidity: ${hum}</h2>
-                </div>
-            </div>
-            <div class="row align-items-start">
-                <div class="col">
-                    <h2>WindSpeed: ${winspe}</h2>
-                </div>
-            </div>
+            `
+            <h2>${place}</h2>
+            <img id="icons" src="${icImg}">
+            <h2>${descrip}</h2>
+            <h2>Temp: ${tp}</h2>
+            <h2>MaxTemp: ${maxtemp}</h2>
+            <h2>MinTemp: ${mintemp}</h2>
+            <h2>Humidity: ${hum}</h2>
+            <h2>WindSpeed: ${winspe}</h2>
             `
             );
+
+            // <div class="row">
+            //     <div class="col">
+            //         <h2>${place}</h2>
+            //         <img id="icons" src="${icImg}">
+            //     </div>
+            // </div>
+            // <div class="row">
+            //     <div class="col">
+            //         <h2>${descrip}</h2>
+            //     </div>
+            // </div>
+            // <div class="row">
+            //     <div class="col">
+            //         <h2>Temp: ${tp}</h2>
+            //         <h2>MaxTemp: ${maxtemp}</h2>
+            //         <h2>MinTemp: ${mintemp}</h2>
+            //         <h2>Humidity: ${hum}</h2>
+            //     </div>
+            // </div>
+            // <div class="row">
+            //     <div class="col">
+            //         <h2>WindSpeed: ${winspe}</h2>
+            //     </div>
+            // </div>
+
             redo.append(
-                `<button type="button" class='btn' id='btn${i}'>
-                    ${place}
+                `
+                <button class='btn' id=btn${i}>
+                ${place}
                 </button>
                 `
             );
@@ -77,65 +100,119 @@ sunshine = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ap
             localStorage.setItem(`btn${i}`, `${place}`)
 
             i++;
+
+            funky3(lat, lon);
     });
 }
 
-//press button
+//function to get buttons ready
+$(document).ready(function(){
+    $(document).on("click", "button.btn", funky2)
+})
 
-$(`.btn`).on("click", funky2);
-
-//function to get Item
-function funky2 (event){
+//function for when city buttons are clicked
+function funky2 (){
     console.log("clicked")
-    event.preventDefault();
-var key = $(this).id;
-console.log(key)
-city = localStorage.getItem(key)
-// sunshine = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    city = $(this).text()
+sunshine = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
-//     infoCard.children().remove();
+    infoCard.children().remove();
 
-//     fetch(sunshine)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-//         place = data.name;
-//         descrip = data.weather[0].description;
-//         ic = data.weather[0].icon;
-//         tp = data.main.temp;
-//         maxtemp = data.main.temp_max;
-//         mintemp = data.main.temp_min;
-//         hum = data.main.humidity;
-//         winspe = data.wind.speed;
+    fetch(sunshine)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+        place = data.name;
+        descrip = data.weather[0].description;
+        ic = data.weather[0].icon;
+        icImg = `http://openweathermap.org/img/w/`+ic+`.png`;
+        tp = conversion(data.main.temp);
+        maxtemp = conversion(data.main.temp_max);
+        mintemp = conversion(data.main.temp_min);
+        hum = data.main.humidity;
+        winspe = data.wind.speed;
+        lat = data.coord.lat.toFixed(2);
+        lon = data.coord.lon.toFixed(2);
 
-//         infoCard.append(
-//             `<div class="row align-items-start">
-//                 <div class="col">
-//                     <h2>${place}</h2>
-//                     <h2>${ic}</h2>
-//                 </div>
-//             </div>
-//             <div class="row align-items-start">
-//                 <div class="col">
-//                     <h2>${descrip}</h2>
-//                 </div>
-//             </div>
-//             <div class="row align-items-start">
-//                 <div class="col">
-//                     <h2>Temp: ${tp}</h2>
-//                     <h2>MaxTemp: ${maxtemp}</h2>
-//                     <h2>MinTemp: ${mintemp}</h2>
-//                     <h2>Humidity: ${hum}</h2>
-//                 </div>
-//             </div>
-//             <div class="row align-items-start">
-//                 <div class="col">
-//                     <h2>WindSpeed: ${winspe}</h2>
-//                 </div>
-//             </div>
-//             `
-//             );
-//         });
+        infoCard.append(
+            `<div class="row">
+                <div class="col">
+                    <h2>${place}</h2>
+                    <img id="icons" src="${icImg}">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h2>${descrip}</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h2>Temp: ${tp}</h2>
+                    <h2>MaxTemp: ${maxtemp}</h2>
+                    <h2>MinTemp: ${mintemp}</h2>
+                    <h2>Humidity: ${hum}</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h2>WindSpeed: ${winspe}</h2>
+                </div>
+            </div>
+            `
+            );
+        });
+        
+        funky3(lat, lon);
+}
+
+//function for the five day forcast
+function funky3(lat, lon){
+
+    // searchBlock.removeClass('container-fluid text-center col-12');
+    // searchBlock.addClass('col-md-12 col-lg-3')
+
+    $('.startBlock').removeClass('text-center')
+    $('.searchBlock').removeClass('col-12');
+    $('.searchBlock').addClass('col-3')
+    $('.searchBlock').attr('style', 'padding-top: 1%; padding-bottom: 16%;')
+
+    futurecard.children().remove();
+
+    fetchFutureURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+    fetch(fetchFutureURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var dayCount = 1;
+        var hourIter = 1;
+        
+        for (var j = 0; j < 5; j++) {
+          var fut = present.add(dayCount, 'day').format("MM/DD/YYYY");
+          dayCount++;
+          var futIc = data.list[hourIter].weather[0].icon;
+          var futIcURL = "http://openweathermap.org/img/w/" + futIc + ".png";
+          var nextTemp = conversion(data.list[hourIter].main.temp);
+          var nextWin = data.list[hourIter].wind.speed;
+          var nextHum = data.list[hourIter].main.humidity;
+          futurecard.append(
+            `
+            <div class="col">
+                <h4>${fut}</h4>
+                <img id="icons" src="${futIcURL}">
+                <p>Temp: ${nextTemp}&#8457;</p>
+                <p>Wind: ${nextWin} MPH</p>
+                <p>Humidity: ${nextHum}%</p>
+            </div>
+            `
+            );
+
+          hourIter = hourIter + 8;
+            }
+        }
+    )
 }
---
+++ README.md
@@ -0,0 +1,35 @@
+# The Weatherton 3000
+
+## Description
+
+The Weatherton 3000 is a mock up Weather Forcast Webpage.
+ - My motive for this project was to make a working Weather Forcast Website that would allow me to plan future road trips.
+ - I built this project to learn more about Jquery, I dived more into using jquery as it is one of my weak points and I had came out a better coder then I was, I also Had to learn how to use insomnia.
+ - This can be used to plan future road trips.
+ - I've learned that things are hard to figure out, and with time, you can do it.
+ - I've also learned more jquery and am more comfortable with it.
+
+## Usage
+
+When the page is loaded up:
+ - it has the a fun header and even funnier footer.
+ - it has a text area and a search button.
+
+ ![alt text](../Assets/Screenshot%202023-10-12%20234905.png)
+
+When I click the button:
+  - it searches for the city that I had put into it.
+
+ ![alt text](../Assets/Screenshot%202023-10-12%20235054.png)
+
+After some seaching,
+ - I can click some buttons with diffrent city names.
+ - When they are clicked they bring back the information on those cities.
+
+## credits
+
+Joey Thao - https://github.com/Thaodev23
+
+Jason Yang - https://github.com/Jasony95
+
+On this homework, We were diving to insomnia, as much as I have insomnia and don't want it, I have this and don't want it. I know that it can be proven very useful and I'm doing my bets to understand it, but I will have to keep trying until I can understand it.
